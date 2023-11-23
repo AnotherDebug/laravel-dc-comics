430a1136d7fa28f69284a71c9b5adf72f98c06f2
AnotherDebug <antonio.faddanno@gmail.com>
2023-11-23 18:51:17 +0100

    [ADD]: content in create blade php

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -3,18 +3,7 @@
 @section('content')
     <h1 class="text-center">Pagina Nuovo Fumetto</h1>
 
-    <div class="container-fluid text-center mb-5">
-        <div class="card m-auto" style="width: 20rem;">
-            <img src="{{$comic->thumb}}" class="card-img-top" alt="{{$comic->title}}">
-            <div class="card-body">
-                <p class="card-title"><span class="fw-bold">Descrizione:</span> {{$comic->description}}</p>
-                <p class="card-title"><span class="fw-bold">Serie:</span> {{$comic->series}}</p>
-                <p class="card-title"><span class="fw-bold">Prezzo:</span> {{$comic->price}}</p>
-                <p class="card-title"><span class="fw-bold">Data di vendita:</span> {{$comic->sale_date}}</p>
-                <p class="card-title"><span class="fw-bold">Tipo:</span> {{$comic->type}}</p>
-            </div>
-        </div>
-    </div>
+
 @endsection
 
 @section('title')
