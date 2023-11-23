452fbbdab5628f497c13c5a31503704738f070fa
AnotherDebug <antonio.faddanno@gmail.com>
2023-11-23 18:04:06 +0100

    [ADD]: navbar in header partials

---
+++ resources/views/_header.blade.php
@@ -0,0 +1,11 @@
+<ul class="nav ms-5 mt-2">
+    <li class="nav-item">
+      <a class="nav-link" aria-current="page" href="{{route('home')}}">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="{{route('comics.index')}}">Comics List</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Contacts</a>
+    </li>
+  </ul>
