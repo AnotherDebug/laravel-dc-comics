a8096c688cad08dbd1ac1e18602f7a75647ba1d0
AnotherDebug <antonio.faddanno@gmail.com>
2023-11-23 16:45:58 +0100

    [INIT]: main e home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('content')
+
+<h1>Pagina Home</h1>
+
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus assumenda laboriosam ipsum eveniet debitis illo iure tempora, nam tempore dolorum fugiat mollitia hic ab omnis odio cum, aliquid rem veniam?</p>
+
+@endsection
