d44df780bc825ba3162e786d3986c3c46b223ca0
AnotherDebug <antonio.faddanno@gmail.com>
2023-11-23 14:45:40 +0100

    [ADD]: import model e foreach loop comics

--- database/seeders/ComicsTableSeeder.php
+++ database/seeders/ComicsTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Comic;
 
 class ComicsTableSeeder extends Seeder
 {
@@ -14,6 +15,23 @@ class ComicsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        //importo il file di dati comics da config e lo salvo in una variabile
+        $comics = config('comics');
+
+        //per ogni elemento dell'array
+        foreach ($comics as $comic) {
+            //creo un nuovo record di tipo comic
+            $item = new Comic();
+            $item->title = $comic['title'];
+            $item->description = $comic['description'];
+            $item->thumb = $comic['thumb'];
+            $item->price = $comic['price'];
+            $item->series = $comic['series'];
+            $item->sale_date = $comic['sale_date'];
+            $item->type = $comic['type'];
+            $item->artists = $comic['artists'];
+            $item->writers = $comic['writers'];
+            $item->save();
+        }
     }
 }
