16e80159e89401e5d82274cde88cebd45626b96d
AnotherDebug <antonio.faddanno@gmail.com>
2023-11-24 15:24:40 +0100

    [ADD]: variable comics count in markup

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
 
-<h1>Pagina Home</h1>
+<h1>Home</h1>
 
-<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus assumenda laboriosam ipsum eveniet debitis illo iure tempora, nam tempore dolorum fugiat mollitia hic ab omnis odio cum, aliquid rem veniam?</p>
+<p>In questo momento ci sono {{$comics}} Fumetti nel Database.</p>
 
 @endsection
 
