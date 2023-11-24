6aaf5482515d5d490ed005d86850ac940674a5c5
AnotherDebug <antonio.faddanno@gmail.com>
2023-11-24 17:24:14 +0100

    [DELETE]: link

--- resources/views/_header.blade.php
+++ resources/views/_header.blade.php
@@ -8,7 +8,4 @@
     <li class="nav-item">
       <a class="nav-link" href="{{route('comics.create')}}">Add new Comic</a>
     </li>
-    <li class="nav-item">
-      <a class="nav-link" href="{{route('comics.edit', $comic)}}">Edit Comic</a>
-    </li>
   </ul>
