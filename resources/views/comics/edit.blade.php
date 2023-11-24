9ee4f75d28e5bc11a8e31ca36bf7988e773b84ca
AnotherDebug <antonio.faddanno@gmail.com>
2023-11-24 17:24:38 +0100

    [ADD]: value in form input

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 
 @section('content')
-    <h1 class="text-center">Edit Comic of: {{$comic->title}}</h1>
+    <h1 class="text-center">Edit Comic of: {{ $comic->title }}</h1>
 
     <div class="container-fluid my-5">
         <div class="row">
@@ -13,32 +13,32 @@
                     @method('PUT')
                     <div class="mb-3">
                         <label for="title" class="form-label">Titolo</label>
-                        <input type="text" name="title" class="form-control" id="title" value="">
+                        <input type="text" name="title" class="form-control" id="title" value="{{$comic->title}}">
                     </div>
                     <div class="mb-3">
                         <label for="thumb" class="form-label">Immagine</label>
-                        <input type="text" name="thumb" class="form-control" id="thumb">
+                        <input type="text" name="thumb" class="form-control" id="thumb" value="{{$comic->thumb}}">
                     </div>
                     <div class="mb-3">
                         <label for="price" class="form-label">Prezzo</label>
-                        <input type="text" name="price" class="form-control" id="price">
+                        <input type="text" name="price" class="form-control" id="price" value="{{$comic->price}}">
                     </div>
                     <div class="mb-3">
                         <label for="series" class="form-label">Serie</label>
-                        <input type="text" name="series" class="form-control" id="series">
+                        <input type="text" name="series" class="form-control" id="series" value="{{$comic->series}}">
                     </div>
                     <div class="mb-3">
                         <label for="sale_date" class="form-label">Data di vendita</label>
-                        <input type="text" name="sale_date" class="form-control" id="sale_date">
+                        <input type="text" name="sale_date" class="form-control" id="sale_date" value="{{$comic->sale_date}}">
                     </div>
                     <div class="mb-3">
                         <label for="type" class="form-label">Tipo</label>
-                        <input type="text" name="type" class="form-control" id="type">
+                        <input type="text" name="type" class="form-control" id="type" value="{{$comic->type}}">
                     </div>
                     <div class="mb-3">
                         <label for="description" class="form-label">Descrizione</label>
-                        <textarea class="form-control" id="description" rows="3" name="description"></textarea>
-                      </div>
+                        <textarea class="form-control" id="description" rows="3" name="description">{{$comic->description}}</textarea>
+                    </div>
 
                     <button type="submit" class="btn btn-primary">Modifica</button>
                 </form>
