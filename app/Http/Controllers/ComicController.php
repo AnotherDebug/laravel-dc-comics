42f5f91d0776ade4153b4e9e35b2ebad608776d5
AnotherDebug <antonio.faddanno@gmail.com>
2023-11-24 17:57:47 +0100

    [ADD]: flashing data

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -108,6 +108,6 @@ class ComicController extends Controller
     {
         $comic->delete();
 
-        return redirect()->route('comics.index');
+        return redirect()->route('comics.index')->with('deleted', "Il Fumetto $comic->title è stato eliminato correttamente");
     }
 }
